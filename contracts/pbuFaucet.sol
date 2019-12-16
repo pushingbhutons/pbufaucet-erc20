1ce30a908f37480235308bf563558360c2d73678
Finley <altimagewritingco@gmail.com>
2019-12-17 00:34:21 +0100

    Update and rename FaucetToken.sol to pbuFaucet.sol

--- contracts/pbuFaucet.sol
+++ contracts/pbuFaucet.sol
@@ -5,14 +5,14 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract FaucetToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+contract pbuFaucet is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
     uint8 public constant DECIMALS = 18;
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
+    uint256 public constant INITIAL_SUPPLY = 1 * (10 ** uint256(DECIMALS));
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("FaucetToken", "FAU", DECIMALS) {
+    constructor () public ERC20Detailed("pbuFaucet", "PBU", DECIMALS) {
     }
 
     function() external {
@@ -20,7 +20,7 @@ contract FaucetToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
     }
 
     function mint(address to, uint256 value) public returns (bool) {
-        require(value <= 10000000 ether, "dont be greedy");
+        require(value <= 10000000 ether, "Don't be greedy");
         _mint(to, value);
         return true;
     }
